f2792ae2410c24437143af3382c0d2439e1ff696
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-08-28 10:55:12 +0530

    Add contextLoads integration test to verify Spring ApplicationContext startup

---
+++ Backend/src/test/java/com/newsnowbackend/NewsNowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsnowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsNowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
