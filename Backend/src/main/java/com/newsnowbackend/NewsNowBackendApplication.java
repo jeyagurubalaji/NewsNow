f61477034f9bc029d8b59ffb0b2b34cb21ced717
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-08-28 10:49:33 +0530

    Initialize NewsNowBackendApplication Spring Boot entry point

---
+++ Backend/src/main/java/com/newsnowbackend/NewsNowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newsnowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsNowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsNowBackendApplication.class, args);
+    }
+
+}
